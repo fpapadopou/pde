90ad012fceec93060a02156d13c5bf5ff8f78ab6
Fotis Papadopoulos <fpapadopou@gmail.com>
2016-09-24 13:19:20 +0300

    Moved index template to dedicated folder

--- wide-app/src/WideBundle/Controller/DefaultController.php
+++ wide-app/src/WideBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('WideBundle:Default:index.html.twig');
+        return $this->render('WideBundle:Index:index.html.twig');
     }
 }
