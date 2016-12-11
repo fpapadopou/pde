14d80d9cb3bec006463ce0d2264846cb5a3878a9
Fotis Papadopoulos <fpapadopou@gmail.com>
2016-12-11 13:23:42 +0200

    Render login errors in landing page

--- wide-app/src/WideBundle/Controller/DefaultController.php
+++ wide-app/src/WideBundle/Controller/DefaultController.php
@@ -9,12 +9,28 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 class DefaultController extends Controller
 {
     /**
+     * Renders the landing page of the application page. If a user previously tried to
+     * log in but failed, the error will be rendered too.
+     *
      * @Route("/")
      * @Method({"GET"})
      */
     public function indexAction()
     {
-        return $this->render('WideBundle:Index:index.html.twig');
+        // Since the login form is rendered in the index page, all data from the last failed login attempt (if any),
+        // such as the login error, should be rendered along with the page.
+        /** @var \Symfony\Component\Security\Http\Authentication\AuthenticationUtils $helper */
+        $helper = $this->get('security.authentication_utils');
+
+        return $this->render(
+            'WideBundle:Index:index.html.twig',
+            [
+                // last username entered by the user (if any)
+                'last_username' => $helper->getLastUsername(),
+                // last authentication error (if any)
+                'last_error' => $helper->getLastAuthenticationError()
+            ]
+        );
     }
 
 }
