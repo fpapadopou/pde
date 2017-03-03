dfcdc2787ca4d259ec93b351aefff164aabe6b23
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-03-03 12:08:25 +0200

    Updated index action

--- wide-app/src/WideBundle/Controller/DefaultController.php
+++ wide-app/src/WideBundle/Controller/DefaultController.php
@@ -5,18 +5,32 @@ namespace WideBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
 
+/**
+ * Class DefaultController
+ * @package WideBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
      * Renders the landing page of the application page. If a user previously tried to
      * log in but failed, the error will be rendered too.
      *
-     * @Route("/")
+     * @Route("/", name="index_page")
      * @Method({"GET"})
      */
     public function indexAction()
     {
+        $current_user = $this->getUser();
+
+        if (is_a($current_user, 'WideBundle\Entity\User')) {
+            /** @var Router $router */
+            $router = $this->get('router');
+            return new RedirectResponse($router->generate('editor'));
+        }
+
         // Since the login form is rendered in the index page, all data from the last failed login attempt (if any),
         // such as the login error, should be rendered along with the page.
         /** @var \Symfony\Component\Security\Http\Authentication\AuthenticationUtils $helper */
