7c0a44d699eafa25f3b5a6da9e814d91b91a016c
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-03-28 15:58:12 +0300

    Updated index js file

--- wide-app/web/js/index.js
+++ wide-app/web/js/index.js
@@ -2,6 +2,7 @@
  * Functions that are necessary in the index page of the application.
  */
 
+// Validates the provided email in the login form of the application
 function validateLoginForm() {
     var emailInput = $('#ceid_email');
     var providedEmail = emailInput.val();
@@ -15,3 +16,10 @@ function validateLoginForm() {
     emailInput.parent().removeClass('has-error').addClass('has-success');
     $('#login-form-button').removeAttr('disabled');
 }
+
+// Binds the login form validation to the form input
+$(document).ready(function() {
+    $('#ceid_email').on('input', function () {
+        validateLoginForm();
+    });
+});
