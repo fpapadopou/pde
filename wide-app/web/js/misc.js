316a3054c035596bafac508f2d80b25e7d10eed6
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-03-28 15:58:39 +0300

    Updated misc.js file
    - Added handler for info modals
    - Enhanced ajax functionality

--- wide-app/web/js/misc.js
+++ wide-app/web/js/misc.js
@@ -2,7 +2,34 @@
  * Miscellaneous functions that are used across the application.
  */
 
+// Triggers an info modal with the provided message.
+function infoModalMessage(message) {
+    $('.modal').modal('hide');
+    $('#info-modal-body').html('<i class="fa fa-times"></i> ' + message);
+    $('#info-modal').modal('show');
+}
+
+// Performs an ajax request - the done callback (successful response handler) is passed as a parameter.
 function doAjaxRequest(url, method, callback, data) {
+    // data is an optional parameter
+    data = data || {};
+    $.ajax({
+        url: url,
+        method: method,
+        data: data
+    }).done(function (response) {
+        if (response.success === true) {
+            callback(response);
+            return;
+        }
+        infoModalMessage(response.error);
+    }).fail(function () {
+        infoModalMessage('An error occurred. Please try again.');
+    });
+}
+
+// Performs an ajax request - the done callback is fully specified.
+function doAjaxRequestWithOutput(url, method, callback, data) {
     // data is an optional parameter
     data = data || {};
     $.ajax({
@@ -12,14 +39,12 @@ function doAjaxRequest(url, method, callback, data) {
     }).done(function (response) {
         callback(response);
     }).fail(function () {
-        $('#info-modal-body').html('<i class="fa fa-times"></i><p>An error occurred. Please try again.</p>');
+        infoModalMessage('An error occurred. Please try again.');
     });
 }
 
-$( document ).ready(function() {
+// Enables tooltips across the site
+$(document).ready(function() {
     // Tooltips must be manually initialized as soon as the DOM is ready.
     $('[data-toggle="tooltip"]').tooltip();
-    $('#ceid_email').on('input', function() {
-        validateLoginForm();
-    });
 });
