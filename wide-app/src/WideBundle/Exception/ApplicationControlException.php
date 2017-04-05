56f72ff09b942eb8019f2f92e4133e9829442c7f
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-04-05 09:43:52 +0300

    Added exception class (missing from previous commit)

---
+++ wide-app/src/WideBundle/Exception/ApplicationControlException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace WideBundle\Exception;
+
+/**
+ * Class ApplicationControlException
+ * Thrown by the application's custom event listeners when their requirements are not met.
+ *
+ * @package WideBundle\Exception
+ */
+class ApplicationControlException extends \Exception
+{
+
+}
