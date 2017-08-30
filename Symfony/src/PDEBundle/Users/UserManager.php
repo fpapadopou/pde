c42e2cc97d4368c4371dfa487a7747871ecf54f5
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-08-30 17:44:53 +0300

    Added UserManager class
    The class is meant for user related operations. Currently, only multiple
    user deletion is supported.

---
+++ Symfony/src/PDEBundle/Users/UserManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PDEBundle\Teams;
+
+use Doctrine\ORM\EntityManager;
+use PDEBundle\Entity\User;
+
+/**
+ * Class UserManager
+ * User management related operations.
+ *
+ * @package PDEBundle\Teams
+ */
+class UserManager
+{
+    /** @var EntityManager $entityManager **/
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Deletes users from the system database. The users must be provided in an array.
+     *
+     * @param array $users
+     * @return array
+     */
+    public function deleteUsers($users)
+    {
+        try {
+            foreach ($users as $user) {
+                $this->entityManager->remove($user);
+            }
+        } catch (\Exception $exception) {
+            return ['success' => false, 'error' => 'Failed to delete one or more users ' . $exception->getMessage()];
+        }
+        return ['success' => true];
+    }
+}
