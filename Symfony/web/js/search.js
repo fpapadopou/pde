5c0275995a41f8489e8fddcdb33d6f107f65cb67
Fotis Papadopoulos <fpapadopou@gmail.com>
2017-09-01 11:35:41 +0300

    Added a confirmation modal for team deletion

--- Symfony/web/js/search.js
+++ Symfony/web/js/search.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
-    // Listener for delete team button
+    // Listener for delete team button, triggers the confirmation modal
     $(".delete-team-btn").click(function () {
+        var targetTeamId = $(this).attr('data-team-id');
+        // change modal button target, pop-up modal
+        $('#team-del-modal-btn').attr('data-team-id', targetTeamId);
+        $('#team-delete-modal').modal('show');
+    });
+
+    // Listener for the confirmation modal button
+    $('#team-del-modal-btn').click(function () {
         var targetTeamId = $(this).attr('data-team-id');
         ajaxRequestWithSuccessHandler(
             deleteTeamUrl,
             'DELETE',
             function () {
+                $('#team-delete-modal').modal('hide');
                 $('#team-' + targetTeamId + '-list-group-item').hide(100);
             },
             {'team' : targetTeamId}
